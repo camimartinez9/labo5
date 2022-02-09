37f43b5477658cab9b028e84e5b0507bf762a67f
camimartinez9 <99033113+camimartinez9@users.noreply.github.com>
2022-02-09 13:56:45 -0300

    Update and rename Nuclear_dia_1.py to Nuclear.py

--- Nuclear.py
+++ Nuclear.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb  2 12:25:53 2022
-
 @author: publico
 """
 
@@ -25,21 +24,21 @@ res = rm.list_resources('@py')
 #%%
 t1, v1 = osc.getWindow(1)
 # t2, v2 = osc.getWindow(2)
-
+plt.figure()
 plt.plot(t1, v1)
 plt.xlabel('Tiempo(s)')
 plt.ylabel('Voltaje(V)')
 plt.grid()
 #Ruido del sistema sin fuente radiactiva
 #%%
-plt.title('Voltaje en función del tiempo-Amplificador')
-plt.plot(t2, v2)
-plt.grid()
-plt.legend()
+# plt.title('Voltaje en función del tiempo-Amplificador')
+# plt.plot(t2, v2)
+# plt.grid()
+# plt.legend()
 #%%
 Picos=[]
-Tiempo_picos=[]
-for i in range(20): #Tomar 20 pantallas
+# Tiempo_picos=[]
+for i in range(2000): #Tomar 20 pantallas
     t1, v1 = osc.getWindow(1)
     # plt.plot(t2, v2)
     # plt.xlabel('Tiempo(s)')
@@ -47,20 +46,34 @@ for i in range(20): #Tomar 20 pantallas
     # # plt.figure(i)
     # plt.grid()
     #picos devuelve los lugares de la fila donde se encuentran efectivamente los picos
-    picos,_=scipy.signal.find_peaks(v1, height=0)#,None,0)  
+    picos,_=scipy.signal.find_peaks(v1, height=0.1)#,None,0)  
     #el guión bajo es para la otra info que da el findpeaks que no me importa
-    tiempo_picos=t1[picos]
+    # tiempo_picos=t1[picos]
     voltaje_picos=v1[picos]
-    Picos.append(voltaje_picos)
-    Tiempo_picos.append(tiempo_picos)
-    cantidad_de_picos=len(picos)
+    for j in voltaje_picos:            
+            Picos.append(j)
+    # Tiempo_picos.append(tiempo_picos)
+    # cantidad_de_picos=len(picos)
     i=i+1
     print(i) #para ir viendo que se está haciendo el loop
-plt.plot(t1,v1)
-plt.plot(Tiempo_picos,Picos,'o',color='r', label = 'Picos')
+# plt.figure()
+# plt.plot(t1,v1)
+# plt.xlabel('Tiempo(s)')
+# plt.ylabel('Voltaje')
+# plt.plot(Tiempo_picos[-1],Picos[-1],'o',color='r', label = 'Picos')
+# plt.legend()
+# plt.grid()
 #Histograma
+#%%
 
-plt.hist(Picos)
+bineado = np.arange(0, 12, 0.16)
+histos, bin_edges = np.histogram(Picos, bineado)
+
+plt.plot(bin_edges[:-1], histos, 'o')
+
+#plt.figure()
+# plt.hist(Picos, bins=bineado)
+plt.yscale('log')
 plt.xlabel('Voltaje')
 plt.ylabel('Cantidad de picos')
 # intervalo=range(min(picos),max(picos)+1)
